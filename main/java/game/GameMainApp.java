f5bc76c19a43f97506019405620377613e44a871
Mateusz Sokol <mateusz.sokol273@gmail.com>
2020-11-01 19:45:33 +0100

    move improvement

--- main/java/game/GameMainApp.java
+++ main/java/game/GameMainApp.java
@@ -12,8 +12,6 @@ public class GameMainApp {
         System.out.println("Hello player, welcome to this cruel world, try to get to '*' symbol to win game," +
                 "u can only step forward or to the left or right u can't go back or neither diagonal, have fun and good luck");
         Scanner scanner = new Scanner(System.in);
-        boolean isFirstRound = true;
-        boolean isSecondRound;
         char[][] chars = {
                 {'O', 'O', 'O', 'O', 'O'},
                 {'O', 'O', 'O', 'O', 'O'},
@@ -36,49 +34,169 @@ public class GameMainApp {
         System.out.println("If you want to go left type '<'");
         System.out.println("If you want to go right type '>'");
         System.out.println("If you want to see items in equipment type 'e'");
+        System.out.println("If you want to exit game type 'quit'");
 
-        while (true) {
+        boolean isStart=true;
+        while (isStart) {
             String command = scanner.nextLine();
             if (command.equalsIgnoreCase("m")) {
                 map.printMap(chars);
-            }
-
-            if (chars[4][4] == character.getCharacterMark() && command.equalsIgnoreCase("^")) {
-                chars[3][4] = character.getCharacterMark();
-                chars[4][4] = 'X';
-
-            } else if (chars[4][4] == character.getCharacterMark() && command.equalsIgnoreCase("<")) {
-                chars[4][3] = character.getCharacterMark();
-                chars[4][4] = 'X';
             } else if (chars[4][4] == character.getCharacterMark() && command.equalsIgnoreCase("e")) {
                 for (int j = 0; j < equipment.size(); j++) {
                     System.out.println(equipment.get(j));
                 }
             } else if (equipment.isEmpty() && command.equalsIgnoreCase("e")) {
                 System.out.println("equipment is empty");
-            } else if (character.getCharacterMark() == chars[3][4] && command.equalsIgnoreCase("^")) {
-                chars[2][4] = character.getCharacterMark();
-                chars[3][4] = 'X';
-            } else if (character.getCharacterMark() == chars[4][3] && command.equalsIgnoreCase("<")) {
-                chars[4][2] = character.getCharacterMark();
-                chars[4][3] = 'X';
-            } else if (character.getCharacterMark() == chars[3][4] && command.equalsIgnoreCase("<") || command.equalsIgnoreCase("^")) {
-                chars[3][3] = character.getCharacterMark();
-                chars[3][4] = 'X';
+            }else if (command.equalsIgnoreCase("quit")){
+                isStart=false;
             }
 
-            String hpPotion = "hp+10";
-            if (character.getCharacterMark() == chars[4][3]) {
-                character.setHp(character.hp + 10);
-                System.out.println("Your hp has been increased by 10, your current hp is: " + character.getHp());
-                equipment.add(hpPotion);
+                if (chars[4][4] == character.getCharacterMark() && command.equalsIgnoreCase("^")) {
+                    chars[3][4] = character.getCharacterMark();
+                    chars[4][4] = 'X';
 
+                } else if (chars[4][4] == character.getCharacterMark() && command.equalsIgnoreCase("<")) {
+                    chars[4][3] = character.getCharacterMark();
+                    chars[4][4] = 'X';
+
+                }else if (chars[3][3]==character.getCharacterMark()&&command.equalsIgnoreCase("^")){
+                    chars[2][3]=character.getCharacterMark();
+                    chars[3][3]='X';
+                }else if (chars[2][3]==character.getCharacterMark()&&command.equalsIgnoreCase("<")){
+                    chars[2][2]=character.getCharacterMark();
+                    chars[2][3]='X';
+                }else if (chars[2][2]==character.getCharacterMark()&&command.equalsIgnoreCase("^")){
+                    chars[1][2]=character.getCharacterMark();
+                    chars[2][2]='X';
+                }else if (chars[1][2]==character.getCharacterMark()&&command.equalsIgnoreCase("<")){
+                    chars[1][1]=character.getCharacterMark();
+                    chars[1][2]='X';
+                }else if (chars[1][1]==character.getCharacterMark()&&command.equalsIgnoreCase("^")){
+                    chars[0][1]=character.getCharacterMark();
+                    chars[1][1]='X';
+
+                }else if (chars[0][1]==character.getCharacterMark()&&command.equalsIgnoreCase("<")){
+                    chars[0][0]=character.getCharacterMark();
+                    chars[0][1]='X';
+                }
+                else if (character.getCharacterMark() == chars[3][4] && command.equalsIgnoreCase("<")) {
+                    chars[3][3] = character.getCharacterMark();
+                    chars[3][4] = 'X';
+                }
+                else if (character.getCharacterMark() == chars[4][3] && command.equalsIgnoreCase("^")) {
+                    chars[3][3] = character.getCharacterMark();
+                    chars[4][3] = 'X';
+                } else if (character.getCharacterMark() == chars[4][3] && command.equalsIgnoreCase("<")) {
+                    chars[4][2] = character.getCharacterMark();
+                    chars[4][3] = 'X';
+                } else if (character.getCharacterMark()==chars[4][2]&&command.equalsIgnoreCase("<")){
+                    chars[4][1]=character.getCharacterMark();
+                    chars[4][2]='X';
+                } else if (character.getCharacterMark() == chars[4][1] && command.equalsIgnoreCase("<")) {
+                    chars[4][0]=character.getCharacterMark();
+                    chars[4][1]='X';
+                }else if (character.getCharacterMark()==chars[4][0]&&command.equalsIgnoreCase("^")){
+                    chars[3][0]=character.getCharacterMark();
+                    chars[4][0]='X';
+                }else if (character.getCharacterMark()==chars[3][0]&&command.equalsIgnoreCase("^")){
+                    chars[2][0]=character.getCharacterMark();
+                    chars[3][0]='X';
+                }else if (character.getCharacterMark()==chars[2][0]&&command.equalsIgnoreCase("^")){
+                    chars[1][0]=character.getCharacterMark();
+                    chars[2][0]='X';
+                }else if (character.getCharacterMark()==chars[1][0]&&command.equalsIgnoreCase("^")){
+                    chars[0][0]=character.getCharacterMark();
+                    chars[1][0]='X';
+                }else if (character.getCharacterMark()==chars[3][3]&&command.equalsIgnoreCase(">")){
+                    chars[3][4]=character.getCharacterMark();
+                    chars[3][3] ='X';
+                }else if (character.getCharacterMark()==chars[3][4]&&command.equalsIgnoreCase("^")){
+                    chars[2][4]=character.getCharacterMark();
+                    chars[3][4]='X';
+                }else if (character.getCharacterMark()==chars[2][4]&&command.equalsIgnoreCase("<")){
+                    chars[2][3]=character.getCharacterMark();
+                    chars[2][4]='X';
+                }else if (character.getCharacterMark()==chars[2][3]&&command.equalsIgnoreCase("^")){
+                    chars[1][3]=character.getCharacterMark();
+                    chars[2][3]='X';
+                }else if (character.getCharacterMark()==chars[1][3]&&command.equalsIgnoreCase(">")){
+                    chars[1][4]=character.getCharacterMark();
+                    chars[1][3]='X';
+                }else if (character.getCharacterMark()==chars[1][4]&&command.equalsIgnoreCase("^")){
+                    chars[0][4]=character.getCharacterMark();
+                    chars[1][4]='X';
+                }else if (character.getCharacterMark()==chars[0][4]&&command.equalsIgnoreCase("<")){
+                    chars[0][3]=character.getCharacterMark();
+                    chars[0][4]='X';
+
+                }else if (character.getCharacterMark()==chars[0][3]&&command.equalsIgnoreCase("<")){
+                    chars[0][2]=character.getCharacterMark();
+                    chars[0][3]='X';
+                }else if (character.getCharacterMark()==chars[0][2]&&command.equalsIgnoreCase("<")){
+                    chars[0][1]=character.getCharacterMark();
+                    chars[0][2]='X';
+                }else if (character.getCharacterMark()==chars[3][3]&&command.equalsIgnoreCase("<")){
+                    chars[3][2]=character.getCharacterMark();
+                    chars[3][3]='X';
+                }else if (character.getCharacterMark()==chars[3][2]&&command.equalsIgnoreCase("<")){
+                    chars[3][1]=character.getCharacterMark();
+                    chars[3][2]='X';
+                }else if (character.getCharacterMark()==chars[3][1]&&command.equalsIgnoreCase("^")){
+                    chars[2][1]=character.getCharacterMark();
+                    chars[3][1]='X';
+
+                }else if (character.getCharacterMark()==chars[2][1]&&command.equalsIgnoreCase("^")){
+                    chars[1][1]=character.getCharacterMark();
+                    chars[2][1]='X';
+                }else if (character.getCharacterMark()==chars[2][3]&&command.equalsIgnoreCase("<")){
+                   chars[2][2]= character.getCharacterMark();
+                   chars[2][3]='X';
+                }else if (character.getCharacterMark()==chars[2][2]&&command.equalsIgnoreCase("<")){
+                    chars[2][1]=character.getCharacterMark();
+                    chars[2][2]='X';
+                }else if (character.getCharacterMark()==chars[3][1]&&command.equalsIgnoreCase("<")){
+                    chars[3][0]=character.getCharacterMark();
+                    chars[3][1]='X';
+                }else if (character.getCharacterMark()==chars[2][1]&&command.equalsIgnoreCase("<")){
+                    chars[2][0]=character.getCharacterMark();
+                    chars[2][1]='X';
+                }else if (character.getCharacterMark()==chars[2][4]&&command.equalsIgnoreCase("^")){
+                    chars[1][4]=character.getCharacterMark();
+                    chars[2][4]='X';
+                }else if (character.getCharacterMark()==chars[1][4]&&command.equalsIgnoreCase("<")){
+                    chars[1][3]=character.getCharacterMark();
+                    chars[1][4]='X';
+                }else if (character.getCharacterMark()==chars[1][3]&&command.equalsIgnoreCase("<")){
+                    chars[1][2]=character.getCharacterMark();
+                    chars[1][3]='X';
+                }else if (character.getCharacterMark()==chars[1][2]&&command.equalsIgnoreCase("<")){
+                    chars[1][1]=character.getCharacterMark();
+                    chars[1][2]='X';
+                }else if (character.getCharacterMark()==chars[1][1]&&command.equalsIgnoreCase("^")){
+                    chars[0][1]=character.getCharacterMark();
+                    chars[1][1]='X';
+                }
+                String hpPotion = "hp+10";
+                if (character.getCharacterMark() == chars[4][3]) {
+                    character.setHp(character.hp + 10);
+                    System.out.println("Your hp has been increased by 10, your current hp is: " + character.getHp());
+                    equipment.add(hpPotion);
+
+                }
+
+
+
+                if (character.getCharacterMark()==chars[0][0]){
+                    System.out.println("u have win congratulation !!");
+                    isStart=false;
+                }
             }
-
         }
 
     }
 
 
-}
+
+
+
 
